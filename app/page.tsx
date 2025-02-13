9ce1938d3d98e460f273f147f4039c586890cce0
Akhil <akhil1659@gmail.com>
2025-02-13 15:59:44 +0530

    changes in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 export default function Home() {
   return (
-    <div className="w-full h-screen bg-gray-900 ">
-      <div className="w-full h-full text-3xl flex justify-center items-start font-bold text-white">
-        Before CI/CD checking
-      </div> 
+    <div className="w-full h-screen bg-gray-900 text-white flex justify-center items-center font-bold text-3xl gap-y-4">
+      <div className="flex flex-col gap-y-4">
+        <div>
+          Before CI/CD checking 
+        </div>
+        <div>
+          CI/CD pipeline completed
+        </div>
+      </div>
     </div>
   );
 }
