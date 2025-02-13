11122dbfb585fa6238c3202b446a4a0602193ef3
Akhil <akhil1659@gmail.com>
2025-02-13 15:54:08 +0530

    changes in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="w-full h-screen bg-gray-900 flex justify-center items-start">
-      <div className="font-bold text-white">
+    <div className="w-full h-screen bg-gray-900 ">
+      <div className="w-full h-full text-3xl flex justify-center items-start font-bold text-white">
         Before CI/CD checking
       </div> 
     </div>
