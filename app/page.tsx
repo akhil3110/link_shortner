1f9e61f2e6cd7125198189edd7b37672c55101c8
Akhil <akhil1659@gmail.com>
2025-02-13 15:46:42 +0530

    production issue resolved

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="w-full h-full flex justify-center items-start">
-      Before CI/CD
+      Before CI/CD checking 
     </div>
   );
 }
